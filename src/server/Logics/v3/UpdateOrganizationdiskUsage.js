409061b6e9005bab980a223164e53700935e1fe8
Ken Yasue <ken.yasue@clover-studio.com>
2017-09-11 23:18:06 +0200

    no message

--- src/server/Logics/v3/UpdateOrganizationdiskUsage.js
+++
@@ -1,8 +0,0 @@
-const _ = require('lodash');
-const UpdateOrganizationDiskUsageV2 = require('../UpdateOrganizationDiskUsage');
-
-const UpdateOrganizationDiskUsage = {};
-
-_.extend(UpdateOrganizationDiskUsage, UpdateOrganizationDiskUsageV2);
-
-module["exports"] = UpdateOrganizationDiskUsage;
