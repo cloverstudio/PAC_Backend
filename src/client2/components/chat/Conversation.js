c3e0d894f59ecda0476fd1671e40a784917648ee
Ken Yasue <ken.yasue@clover-studio.com>
2017-12-05 14:52:33 +0100

    save work

--- src/client2/components/chat/Conversation.js
+++ src/client2/components/chat/Conversation.js
@@ -96,6 +96,9 @@ class Conversation extends Component {
         for(let i = 0 ; i < this.props.messageList.length ; i++){
 
             const message = this.props.messageList[i];
+
+            console.log('message',message);
+            
             const currentUser = message.userID;
             const messageDate = new Date(message.created).getDate();
 
