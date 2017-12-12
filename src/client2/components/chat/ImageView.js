1e311cef62de949fdea1107c8608337397d46b88
rpavlini <robert.pavlinic95@gmail.com>
2017-12-12 18:28:42 +0100

    img viewer

--- src/client2/components/chat/ImageView.js
+++ src/client2/components/chat/ImageView.js
@@ -5,22 +5,59 @@ import { Link } from 'react-router-dom';
 
 import * as constant from '../../lib/const';
 import * as actions from '../../actions';
-
+import * as config from '../../lib/config';
 
 class ImageView extends Component {
 
+    constructor(){
+        super();
+        this.state = {
+            isLoading: true
+        }
+    }
     static propTypes = {
     }
 
+    toggleImgLoading = () => {
+        this.setState({
+            isLoading : !this.state.isLoading
+        })
+    }
+
     render() {
-        // return (
-            
-        //     // <div className={}> 
-
-        //     // </div>
-
-        // );
-        return null;
+        const mainStyle = {
+            display: this.props.visibility ? 'block' : 'none'
+        }
+        return(
+        <div className="modal-open">
+            <div className="modal modal-center fade show image-view" id="modal-center" style={mainStyle}>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                        <button type="button" className="close" data-dismiss="modal" 
+                        onClick={e => {
+                            this.props.hideImageView(); 
+                            this.toggleImgLoading()
+                            }}>
+                            <span aria-hidden="true">×</span>
+                        </button>
+                        </div>
+                        <div className="modal-body">
+                            {this.state.isLoading ?
+                            <span className="spinner-linear">
+                                <span className="line"></span>
+                            </span> : null}
+                            {this.props.visibility ? 
+                            <img src={config.APIEndpoint + constant.ApiUrlFile + this.props.imgId}
+                            alt="image"
+                            onLoad={e=> this.toggleImgLoading()}/> : null}
+                        </div>
+                
+                    </div>
+                </div>
+            </div>
+            {this.props.visibility ? <div className="modal-backdrop fade show"></div>:null } 
+        </div> )
     }
 
 }
@@ -84,11 +121,14 @@ class ImageView extends Component {
 
 const mapStateToProps = (state) => {
     return {
+        visibility: state.chatUI.imageViewState,
+        imgId: state.imageView.imgId
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
+        hideImageView: () => dispatch(actions.chatUI.hideImageView())
     };
 };
 
