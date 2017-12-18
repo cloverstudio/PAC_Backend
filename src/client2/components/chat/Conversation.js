85b13ba1e0caea0a4b5f7f37dd7d2d8b83c43e1f
Ken Yasue <ken.yasue@clover-studio.com>
2017-12-18 11:38:00 +0100

    some fixes

--- src/client2/components/chat/Conversation.js
+++ src/client2/components/chat/Conversation.js
@@ -1,272 +1,287 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import * as actions from '../../actions';
-import * as constant from '../../lib/const';
-import * as strings from '../../lib/strings';
-import * as config from '../../lib/config';
-import * as util from '../../lib/utils';
+import * as actions from "../../actions";
+import * as constant from "../../lib/const";
+import * as strings from "../../lib/strings";
+import * as config from "../../lib/config";
+import * as util from "../../lib/utils";
 
-import user from '../../lib/user';
+import user from "../../lib/user";
 
-import SocketManager from '../../lib/SocketManager';
-import Encryption from '../../lib/encryption/encryption';
-import AvatarImage from '../AvatarImage';
-import DateTime from '../DateTime';
-import Stickers from './Stickers';
-import ChatInput from './ChatInput';
-import ChatHeader from './ChatHeader';
-import Message from './Message';
+import SocketManager from "../../lib/SocketManager";
+import Encryption from "../../lib/encryption/encryption";
+import AvatarImage from "../AvatarImage";
+import DateTime from "../DateTime";
+import Stickers from "./Stickers";
+import ChatInput from "./ChatInput";
+import ChatHeader from "./ChatHeader";
+import Message from "./Message";
 
 class Conversation extends Component {
-    constructor(){
-        super()
-        
-        this.todayDateObject = new Date();
-        this.todayDate = `${this.todayDateObject.getFullYear()}-${this.todayDateObject.getMonth()}-${this.todayDateObject.getDate()}`
-        this.stickersLoaded = false;
-        this.lockedForAutoScroll = false;
+  constructor() {
+    super();
+
+    this.todayDateObject = new Date();
+    this.todayDate = `${this.todayDateObject.getFullYear()}-${this.todayDateObject.getMonth()}-${this.todayDateObject.getDate()}`;
+    this.stickersLoaded = false;
+    this.lockedForAutoScroll = false;
+  }
+
+  static propTypes = {};
+
+  onScroll = e => {
+    if (e.target.scrollTop === 0 && !this.props.isLoading) {
+      this.props.loadOldMessages(
+        this.props.currentChatId,
+        this.props.messageList[0]._id
+      );
     }
 
-    static propTypes = {
+    this.lockedForAutoScroll =
+      util.getScrollBottom(this.scrollableConversation) >
+      constant.scrollBoundary;
+  };
+
+  componentWillUpdate(nextProps) {
+    this.lastConversationHeight = this.scrollableConversation.scrollHeight;
+    this.lastConversationScrollTop = this.scrollableConversation.scrollTop;
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.lockedForAutoScroll) {
+      this.scrollableConversation.scrollTop +=
+        this.scrollableConversation.scrollHeight - this.lastConversationHeight;
+    } else {
+      if (
+        this.scrollableConversation.scrollHeight !== this.lastConversationHeight
+      ) {
+        util.scrollElemBottom(this.scrollableConversation);
+      }
+    }
+  }
+
+  scrollCallback = e => {
+    if (!this.lockedForAutoScroll) {
+      util.scrollElemBottom(this.scrollableConversation);
+    }
+  };
+
+  handleDragEnter = e => {
+    e.preventDefault();
+
+    if (e.target.classList.contains("chat-content")) {
+      e.target.classList.add("dragging-over");
+      this.props.showDropNotification("Drop files now");
+    }
+  };
+
+  handleDrop = e => {
+    e.preventDefault();
+    Array.from(e.dataTransfer.files).forEach(file => {
+      const data = new FormData();
+      data.append("file", file);
+      this.props.startFileUpload(data);
+    });
+    if (e.target.classList.contains("chat-content")) {
+      e.target.classList.remove("dragging-over");
+      this.props.hideDropNotification();
+    }
+  };
+
+  handleDragLeave = e => {
+    e.preventDefault();
+    if (e.target.classList.contains("chat-content")) {
+      e.target.classList.remove("dragging-over");
+      this.props.hideDropNotification();
+    }
+  };
+
+  handleDragOver = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    var sortedMsgs = new Map();
+    let messageList = [...this.props.messageList];
+
+    if (typeof this.props.files[this.props.currentChatId] !== "undefined") {
+      let filesInProgress = Object.values(
+        this.props.files[this.props.currentChatId]
+      );
+      filesInProgress = filesInProgress.filter(
+        file =>
+          this.props.messageList.find(msg => msg.localID === file.localID) ===
+          undefined
+      );
+
+      messageList = messageList.concat(filesInProgress);
     }
 
-    onScroll = (e) => {  
-        if (e.target.scrollTop === 0 && !this.props.isLoading){
-            this.props.loadOldMessages(this.props.currentChatId, this.props.messageList[0]._id)
+    if (messageList.length > 0) {
+      let currUsr = messageList[0].userID;
+      let currDatObject = new Date(messageList[0].created);
+      //YYYY-MM-DD
+      let currDat = `${currDatObject.getFullYear()}-${currDatObject.getMonth()}-${currDatObject.getDate()}`;
+      let currMsgs = [];
+
+      sortedMsgs.set(currDat, []);
+
+      for (let msg of messageList) {
+        let msgUsr = msg.userID;
+        let msgDatObject = new Date(msg.created);
+        //YYYY-MM-DD
+        let msgDat = `${msgDatObject.getFullYear()}-${msgDatObject.getMonth()}-${msgDatObject.getDate()}`;
+
+        if (currDat !== msgDat) {
+          if (currMsgs.length > 0) sortedMsgs.get(currDat).push(currMsgs);
+          currMsgs = [];
+          sortedMsgs.set(msgDat, []);
         }
-        
-        this.lockedForAutoScroll = util.getScrollBottom(this.scrollableConversation) > constant.scrollBoundary;
-    }
 
-    componentWillUpdate(nextProps){
-        this.lastConversationHeight = this.scrollableConversation.scrollHeight;
-        this.lastConversationScrollTop = this.scrollableConversation.scrollTop;
-    }
-
-    componentDidUpdate(prevProps){
-            if (this.lockedForAutoScroll){
-                this.scrollableConversation.scrollTop += this.scrollableConversation.scrollHeight - this.lastConversationHeight;
-            }
-            else{
-                if(this.scrollableConversation.scrollHeight !== this.lastConversationHeight){
-                    util.scrollElemBottom(this.scrollableConversation);
-                }
-            }
-    }
-
-    scrollCallback = e => {
-       if(!this.lockedForAutoScroll){
-            util.scrollElemBottom(this.scrollableConversation)
+        if (currUsr !== msgUsr) {
+          if (currMsgs.length > 0) sortedMsgs.get(currDat).push(currMsgs);
+          currMsgs = [];
         }
+
+        currMsgs.push(msg);
+        currUsr = msgUsr;
+        currDat = msgDat;
+      }
+
+      sortedMsgs.get(currDat).push(currMsgs);
     }
 
-    handleDragEnter = e => {
-        e.preventDefault();
+    let conversationItems = [];
 
-        if (e.target.classList.contains('chat-content')){
-            e.target.classList.add('dragging-over');
-            this.props.showDropNotification('Drop files now');
-
-        }
-    }
-
-    handleDrop = e => {
-        e.preventDefault();
-        Array.from(e.dataTransfer.files).forEach( file => {
-            const data = new FormData();
-            data.append('file', file);
-            this.props.startFileUpload(data);
-        });
-        if (e.target.classList.contains('chat-content')){
-            e.target.classList.remove('dragging-over');
-            this.props.hideDropNotification();
-        }
-       
-    }
-
-    handleDragLeave = e => {
-        e.preventDefault();
-        if (e.target.classList.contains('chat-content')){
-            e.target.classList.remove('dragging-over');
-            this.props.hideDropNotification();
-        }
-    }
-
-    handleDragOver = e => {
-        e.preventDefault();
-    }
-    
-    render() {
-
-        var sortedMsgs = new Map();
-        let messageList = [...this.props.messageList];
-
-        if(typeof this.props.files[this.props.currentChatId] !== 'undefined'){
-        
-            let filesInProgress = Object.values(this.props.files[this.props.currentChatId]);
-            filesInProgress = filesInProgress.filter(file => this.props.messageList.find(msg => msg.localID === file.localID) === undefined )
-
-            messageList = messageList.concat(filesInProgress);
-        }
-        
-        if (messageList.length > 0){
-
-            let currUsr = messageList[0].userID;
-            let currDatObject = new Date(messageList[0].created);
-            //YYYY-MM-DD
-            let currDat = `${currDatObject.getFullYear()}-${currDatObject.getMonth()}-${currDatObject.getDate()}`
-            let currMsgs = [];
-            
-            sortedMsgs.set(currDat, []);
-            
-            for (let msg of messageList){
-                let msgUsr = msg.userID;
-                let msgDatObject = new Date(msg.created);
-                //YYYY-MM-DD
-                let msgDat = `${msgDatObject.getFullYear()}-${msgDatObject.getMonth()}-${msgDatObject.getDate()}`
-                
-                if (currDat !== msgDat){
-                    if (currMsgs.length > 0) sortedMsgs.get(currDat).push(currMsgs);
-                    currMsgs = [];
-                    sortedMsgs.set(msgDat, []);
-                }
-                
-                if (currUsr !== msgUsr){
-                    if (currMsgs.length > 0) sortedMsgs.get(currDat).push(currMsgs);
-                    currMsgs = [];
-                }
-                
-                currMsgs.push(msg);
-                currUsr = msgUsr;
-                currDat = msgDat;
-            }
-
-            sortedMsgs.get(currDat).push(currMsgs);
-        }
-        
-        let conversationItems = [];        
-
-        sortedMsgs.forEach( (messagesByDate, date) => {
-
-            if (this.todayDate === date) {conversationItems.push(<div key={date} className='media media-meta-day'>Today</div>);}
-            
-            messagesByDate.forEach( messagesByUser => {
-                
-                const firstMessage = messagesByUser[0];
-                const lastMessage = messagesByUser[messagesByUser.length-1];
-                const user = firstMessage.user;
-                const userId = user ? user._id : firstMessage.userID;
-
-                let groupedMessages;
-
-                if (userId === this.props.user._id){
-                    groupedMessages = (
-                    <div className='media media-chat mymessage' key={firstMessage._id || firstMessage.localID}>
-
-                        <div className='media-body'>
-                            
-                            {messagesByUser.map( message => <Message key={message._id || message.localID} messageData={message} scrollChat={this.scrollCallback}/>)}
-                            
-                            <p className="meta">
-                                <DateTime timestamp={lastMessage.created} />
-                            </p>
-
-                        </div>
-
-                    </div>)
-                }
-                else {
-                    groupedMessages = (
-                    <div className='media media-chat' key={firstMessage._id}>
-                        
-                        <AvatarImage type={constant.AvatarUser} user={user} />
-                        
-                        <div className='media-body'>
-                            
-                            {messagesByUser.map( message => <Message key={message._id} messageData={message} scrollChat={this.scrollCallback}/>)}
-                            
-                            <p className="meta">
-                                <DateTime timestamp={lastMessage.created} />
-                            </p>
-
-                        </div>
-
-                    </div>)
-                }
-
-                conversationItems.push(groupedMessages)
-
-            })
-                                    
-        })
-        
-        let chatContainerClass = "chat-container card card-bordered flex-column";
-        if(this.props.infoViewState)
-            chatContainerClass += " hide";
-
-        return (
-            
-            <div className={chatContainerClass}>
-                
-
-                    {this.props.isLoading ?
-                        <div className="spinner-linear">
-                            <div className="line"></div>
-                        </div> : null
-                    }
-
-                    <ChatHeader/>
-
-                    <div ref={ (scrollableConversation) => { this.scrollableConversation = scrollableConversation}} 
-                    onScroll={ this.onScroll }
-                    onDragEnter={this.handleDragEnter}
-                    onDrop={this.handleDrop} 
-                    onDragLeave={this.handleDragLeave}
-                    onDragOver={this.handleDragOver}                
-                    className="scrollable flex-grow chat-content">
-
-                        {conversationItems}
-                    
-                    </div> 
-
-                    <ChatInput/>
-                    <Stickers/>
-                
-
-
-            </div>
-
+    sortedMsgs.forEach((messagesByDate, date) => {
+      if (this.todayDate === date) {
+        conversationItems.push(
+          <div key={date} className="media media-meta-day">
+            Today
+          </div>
         );
-    }
+      }
 
+      messagesByDate.forEach(messagesByUser => {
+        const firstMessage = messagesByUser[0];
+        const lastMessage = messagesByUser[messagesByUser.length - 1];
+        const user = firstMessage.user;
+        const userId = user ? user._id : firstMessage.userID;
+
+        let groupedMessages;
+
+        if (userId === this.props.user._id) {
+          groupedMessages = (
+            <div
+              className="media media-chat mymessage"
+              key={firstMessage._id || firstMessage.localID}
+            >
+              <div className="media-body">
+                {messagesByUser.map(message => (
+                  <Message
+                    key={message._id || message.localID}
+                    messageData={message}
+                    scrollChat={this.scrollCallback}
+                  />
+                ))}
+
+                <p className="meta">
+                  <DateTime timestamp={lastMessage.created} />
+                </p>
+              </div>
+            </div>
+          );
+        } else {
+          groupedMessages = (
+            <div className="media media-chat" key={firstMessage._id}>
+              <AvatarImage type={constant.AvatarUser} user={user} />
+
+              <div className="media-body">
+                {messagesByUser.map(message => (
+                  <Message
+                    key={message._id}
+                    messageData={message}
+                    scrollChat={this.scrollCallback}
+                  />
+                ))}
+
+                <p className="meta">
+                  <DateTime timestamp={lastMessage.created} />
+                </p>
+              </div>
+            </div>
+          );
+        }
+
+        conversationItems.push(groupedMessages);
+      });
+    });
+
+    let chatContainerClass = "chat-container card card-bordered flex-column";
+    if (this.props.infoViewState) chatContainerClass += " hide";
+
+    return (
+      <div className={chatContainerClass}>
+        {!this.props.currentChatId ? (
+          <div className="callout callout-info" role="alert">
+            <h5>{strings.ChatWelcomeTitle[user.lang]}</h5>
+            <p>{strings.ChatWelcomeText[user.lang]}</p>
+          </div>
+        ) : null}
+
+        {this.props.isLoading ? (
+          <div className="spinner-linear">
+            <div className="line" />
+          </div>
+        ) : null}
+
+        <ChatHeader />
+        <div
+          ref={scrollableConversation => {
+            this.scrollableConversation = scrollableConversation;
+          }}
+          onScroll={this.onScroll}
+          onDragEnter={this.handleDragEnter}
+          onDrop={this.handleDrop}
+          onDragLeave={this.handleDragLeave}
+          onDragOver={this.handleDragOver}
+          className="scrollable flex-grow chat-content"
+        >
+          {conversationItems}
+        </div>
+
+        <ChatInput />
+        <Stickers />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        currentChatId:state.chat.chatId,
-        chatAvatar:state.chat.chatAvatar,
-        isLoading:state.chat.isLoading,
-        messageList: state.chat.messageList,
-        user:user.userData,
-        UsersTyping: state.chat.typing,
-        infoViewState: state.chatUI.infoViewState,
-        files: state.files
-    };
+const mapStateToProps = state => {
+  return {
+    currentChatId: state.chat.chatId,
+    chatAvatar: state.chat.chatAvatar,
+    isLoading: state.chat.isLoading,
+    messageList: state.chat.messageList,
+    user: user.userData,
+    UsersTyping: state.chat.typing,
+    infoViewState: state.chatUI.infoViewState,
+    files: state.files
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        loadOldMessages: (chatID, lastMessage) => dispatch(actions.chat.loadOldMessages(chatID, lastMessage)),
-        startFileUpload: file => dispatch(actions.chat.startFileUpload(file)),
-        showDropNotification: message => dispatch(actions.notification.showToast(message)),
-        hideDropNotification: () => dispatch(actions.notification.hideToast())
-    };
+const mapDispatchToProps = dispatch => {
+  return {
+    loadOldMessages: (chatID, lastMessage) =>
+      dispatch(actions.chat.loadOldMessages(chatID, lastMessage)),
+    startFileUpload: file => dispatch(actions.chat.startFileUpload(file)),
+    showDropNotification: message =>
+      dispatch(actions.notification.showToast(message)),
+    hideDropNotification: () => dispatch(actions.notification.hideToast())
+  };
 };
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Conversation);
+export default connect(mapStateToProps, mapDispatchToProps)(Conversation);
--- src/client2/components/chat/Information.js
+++ src/client2/components/chat/Information.js
@@ -1,88 +1,77 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import * as constant from '../../lib/const';
-import * as actions from '../../actions';
+import * as constant from "../../lib/const";
+import * as actions from "../../actions";
 
-import AvatarImage from '../AvatarImage';
+import AvatarImage from "../AvatarImage";
 
-import UserInfo from './infoview/UserInfo';
-import GroupInfo from './infoview/GroupInfo';
-import RoomInfo from './infoview/RoomInfo';
+import UserInfo from "./infoview/UserInfo";
+import GroupInfo from "./infoview/GroupInfo";
+import RoomInfo from "./infoview/RoomInfo";
 
 class Information extends Component {
+  static propTypes = {};
 
-    static propTypes = {
-    }
+  //hidden-xs-down
+  render() {
+    let infoContainerClass = "info-container bg-lighter border-light ";
+    if (this.props.infoViewState) infoContainerClass += " show";
 
-    //hidden-xs-down
-    render() {
+    return (
+      <div className={infoContainerClass}>
+        {this.props.currentChatId ? (
+          <div className="quickview-body ps-container ps-theme-default">
+            {this.props.isLoading ? (
+              <div className="spinner-linear">
+                <div className="line" />
+              </div>
+            ) : null}
 
-        let infoContainerClass = "info-container bg-lighter border-light ";
-        if(this.props.infoViewState)
-            infoContainerClass += " show";
+            <div className="card card-inverse bg-img">
+              <div className="card-body text-center pt-50 pb-50">
+                <AvatarImage
+                  className="avatar avatar-xxl avatar-bordered"
+                  fileId={this.props.chatAvatar.fileId}
+                  type={this.props.chatAvatar.type}
+                />
 
-        return (
-            
-            <div className={infoContainerClass}> 
-
-
-                {this.props.isLoading ?
-                    <div className="spinner-linear">
-                        <div className="line"></div>
-                    </div> : null
-                }
-                
-                <div className="quickview-body ps-container ps-theme-default">
-
-                    <div className="card card-inverse bg-img" >
-
-                        <div className="card-body text-center pt-50 pb-50">
-                            
-                            <AvatarImage 
-                                className="avatar avatar-xxl avatar-bordered"
-                                fileId={this.props.chatAvatar.fileId} 
-                                type={this.props.chatAvatar.type} 
-                            />
-
-                            <h4 className="mt-2 mb-0">
-                                <span className="hover-primary text-white">{this.props.chatAvatar.name}</span>
-                            </h4>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                {this.props.chatType == constant.ChatTypePrivate ? <UserInfo /> : null }
-                {this.props.chatType == constant.ChatTypeGroup ? <GroupInfo /> : null }
-                {this.props.chatType == constant.ChatTypeRoom ? <RoomInfo /> : null }
-
+                <h4 className="mt-2 mb-0">
+                  <span className="hover-primary text-white">
+                    {this.props.chatAvatar.name}
+                  </span>
+                </h4>
+              </div>
             </div>
 
-        );
-    }
-
+            {this.props.chatType == constant.ChatTypePrivate ? (
+              <UserInfo />
+            ) : null}
+            {this.props.chatType == constant.ChatTypeGroup ? (
+              <GroupInfo />
+            ) : null}
+            {this.props.chatType == constant.ChatTypeRoom ? <RoomInfo /> : null}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        chatType: state.chat.chatType,
-        chatAvatar: state.chat.chatAvatar,
-        isLoading: state.infoView.isLoading,
-        infoViewState: state.chatUI.infoViewState
-    };
+const mapStateToProps = state => {
+  return {
+    chatType: state.chat.chatType,
+    chatAvatar: state.chat.chatAvatar,
+    isLoading: state.infoView.isLoading,
+    infoViewState: state.chatUI.infoViewState,
+    currentChatId: state.chat.chatId
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-    };
+const mapDispatchToProps = dispatch => {
+  return {};
 };
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Information);
+export default connect(mapStateToProps, mapDispatchToProps)(Information);
