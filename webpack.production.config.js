c473352bdf068ea0dd555c6ae9c969f3f36b19f0
Ken Yasue <ken.yasue@clover-studio.com>
2018-01-05 09:36:15 +0100

    no message

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -89,7 +89,7 @@ module.exports = {
         loader: "url?limit=10000&mimetype=application/font-woff"
       },
       {
-        test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/,
+        test: /\.(ttf|eot|svg|mp3)(\?[a-z0-9#=&.]+)?$/,
         loader: "file"
       }
     ]
