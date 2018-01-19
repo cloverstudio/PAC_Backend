ab33e8e996eeaa6120380e88b11e883f6a2ee80e
Jurica Blazevic <jurica.blazevic@clover-studio.com>
2018-01-19 15:39:11 +0100

    test socket deliver

--- src/server/test/webSocket_06_delivermessage.js
+++ src/server/test/webSocket_06_delivermessage.js
@@ -5,6 +5,7 @@ var app = require('../mainTest');
 var io = require('socket.io-client');
 var _ = require('lodash');
 var request = require('supertest');
+var Const = require("../lib/consts");
 
 describe('SOCKET', function () {
 
@@ -19,7 +20,7 @@ describe('SOCKET', function () {
         it('works for private chat', function (done) {
 
             var params = {
-                token: global.user2.accessToken
+                token: global.user1.accessToken
             };
 
             var client1 = io.connect(socketURL, connectOptions);
@@ -34,7 +35,7 @@ describe('SOCKET', function () {
 
                 client1.emit('deliverMessage', {
                     userID: global.user2._id,
-                    messageID: global.message1._id
+                    messageID: global.privateMessage._id
                 });
 
             });
@@ -55,13 +56,234 @@ describe('SOCKET', function () {
 
             });
 
+        });
+
+        it('works for group chat', function (done) {
+
+            var params = {
+                token: global.user1.accessToken
+            };
+
+            var client1 = io.connect(socketURL, connectOptions);
+
+            client1.on('connect', function (data) {
+
+                client1.emit('login', params);
+
+            });
+
+            client1.on('logined', function () {
+
+                client1.emit('deliverMessage', {
+                    userID: global.user2._id,
+                    messageID: global.groupMessage._id
+                });
+
+            });
+
+            client1.on('updatemessages', function (param) {
+
+                param.should.Array();
+                param.should.have.length(1);
+
+                param[0].deliveredTo.should.Array();
+                param[0].deliveredTo.should.have.length(2);
+
+                param[0].deliveredTo[0].userId.should.equal(global.user2._id);
+
+                client1.disconnect();
+
+                done();
+
+            });
 
         });
 
+        it('works for room chat', function (done) {
+
+            var params = {
+                token: global.user1.accessToken
+            };
+
+            var client1 = io.connect(socketURL, connectOptions);
+
+            client1.on('connect', function (data) {
+
+                client1.emit('login', params);
+
+            });
+
+            client1.on('logined', function () {
+
+                client1.emit('deliverMessage', {
+                    userID: global.user2._id,
+                    messageID: global.roomMessage._id
+                });
+
+            });
+
+            client1.on('updatemessages', function (param) {
+
+                param.should.Array();
+                param.should.have.length(1);
+
+                param[0].deliveredTo.should.Array();
+                param[0].deliveredTo.should.have.length(3);
+
+                param[0].deliveredTo[0].userId.should.equal(global.user2._id);
+
+                client1.disconnect();
+
+                done();
+
+            });
+
+        });
+
+        it('fails if no userId', function (done) {
+
+            var params = {
+                token: global.user1.accessToken
+            };
+
+            var client1 = io.connect(socketURL, connectOptions);
+
+            client1.on('connect', function (data) {
+
+                client1.emit('login', params);
+
+            });
+
+            client1.on('logined', function () {
+
+                client1.emit('deliverMessage', {
+                    userID: "",
+                    messageID: global.roomMessage._id
+                });
+
+            });
+
+            client1.on('socketerror', function (param) {
+
+                param.should.have.property('code');
+                param.code.should.equal(Const.resCodeSocketDeliverMessageNoUserId);
+
+                client1.disconnect();
+
+                done();
+
+            });
+
+        });
+
+        it('fails if no messageId', function (done) {
+
+            var params = {
+                token: global.user1.accessToken
+            };
+
+            var client1 = io.connect(socketURL, connectOptions);
+
+            client1.on('connect', function (data) {
+
+                client1.emit('login', params);
+
+            });
+
+            client1.on('logined', function () {
+
+                client1.emit('deliverMessage', {
+                    userID: global.user2._id,
+                    messageID: ""
+                });
+
+            });
+
+            client1.on('socketerror', function (param) {
+
+                param.should.have.property('code');
+                param.code.should.equal(Const.resCodeSocketDeliverMessageNoMessageId);
+
+                client1.disconnect();
+
+                done();
+
+            });
+
+        });
+
+        it('fails if invalid userId', function (done) {
+
+            var params = {
+                token: global.user1.accessToken
+            };
+
+            var client1 = io.connect(socketURL, connectOptions);
+
+            client1.on('connect', function (data) {
+
+                client1.emit('login', params);
+
+            });
+
+            client1.on('logined', function () {
+
+                client1.emit('deliverMessage', {
+                    userID: "invaliduserId",
+                    messageID: global.roomMessage._id
+                });
+
+            });
+
+            client1.on('socketerror', function (param) {
+
+                param.should.have.property('code');
+                param.code.should.equal(Const.resCodeSocketDeliverMessageWrongUserId);
+
+                client1.disconnect();
+
+                done();
+
+            });
+
+        });
+
+        it('fails if invalid messageId', function (done) {
+
+            var params = {
+                token: global.user1.accessToken
+            };
+
+            var client1 = io.connect(socketURL, connectOptions);
+
+            client1.on('connect', function (data) {
+
+                client1.emit('login', params);
+
+            });
+
+            client1.on('logined', function () {
+
+                client1.emit('deliverMessage', {
+                    userID: global.user2._id,
+                    messageID: "invalidmessageId"
+                });
+
+            });
+
+            client1.on('socketerror', function (param) {
+
+                param.should.have.property('code');
+                param.code.should.equal(Const.resCodeSocketDeliverMessageWrongMessageId);
+
+                client1.disconnect();
+
+                done();
+
+            });
+
+        });
 
     });
-
-
-
 
 });
