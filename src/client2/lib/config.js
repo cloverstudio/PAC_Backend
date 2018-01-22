3e050683e8906ea57e3e3ee0c1ac844886d84ca8
Ken Yasue <ken.yasue@clover-studio.com>
2018-01-22 14:31:02 +0100

    no message

--- src/client2/lib/config.js
+++ src/client2/lib/config.js
@@ -1,14 +1,14 @@
 export const AppTitle = "Spika for Business";
-export const APIEndpoint = "http://dev.spika.chat/api/v2";
+export const APIEndpoint = "https://dev.spika.chat/api/v2";
 export const SignalingServerURL = "https://dev.spika.chat";
 export const SpikaBaseURL = "spika";
 export const defaultContaier = "body";
-export const socketUrl = "http://dev.spika.chat/spikaenterprise";
+export const socketUrl = "https://dev.spika.chat/spikaenterprise";
 export const SpikaSocketURL = "/spika";
 export const hashSalt = "8zgqvU6LaziThJI1uz3PevYd";
 export const AESPassword = "cl0v3r-S+uD10-h4X0r1";
 export const phoneNumberSignin = false;
-export const mediaBaseURL = "http://dev.spika.chat";
+export const mediaBaseURL = "https://dev.spika.chat";
 
 export const BasePath = "";
 export const lang = "en";
