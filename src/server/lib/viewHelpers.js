11a9203155ce796459c53043fb3b0a0bca8c9b40
Ken Yasue <ken.yasue@clover-studio.com>
2018-01-26 09:28:48 +0100

    no message

--- src/server/lib/viewHelpers.js
+++ src/server/lib/viewHelpers.js
@@ -397,6 +397,13 @@ var helpers = {
         }
 
 
+    },
+    "isEqual": function (val1, val2, options) {
+
+        if (val1 == val2)
+            return options.fn(this);
+        else
+            return "";
     }
 }
 
