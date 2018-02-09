0bb66551eec6860e3b869f1745753d2eaddc4ae0
Ken Yasue <ken.yasue@clover-studio.com>
2018-02-09 15:39:05 +0100

    update web connector

--- public/connector/loader/loader.js
+++ public/connector/loader/loader.js
@@ -1,18 +1,16 @@
-(function () {
-
+(function() {
+    
     // settings
     var baseURL = "https://spika.chat/connector/";
 
-    var img = new Image(50, 50); // width, height values are optional params 
+    var img = new Image(50,50); // width, height values are optional params 
     img.src = baseURL + "loader/initial-icon.svg";
-
+    
     img.style.position = "fixed";
-    img.style.bottom = "50px";
-    img.style.right = "50px";
     img.style.zIndex = 99999;
     img.style.cursor = "pointer";
 
-    img.onclick = function () {
+    img.onclick = function(){
 
         img.style.display = "none";
 
@@ -20,19 +18,27 @@
         iframe.src = baseURL;
 
         iframe.style.position = "fixed";
-        iframe.style.height = "100vh";
-        iframe.style.width = "500px";
 
-        iframe.style.top = "0px";
-        iframe.style.right = "50px";
+
+        if( /Android|iPhone|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            iframe.style.width = "90vw";
+            iframe.style.left = "50px";
+            iframe.style.top = "0px";
+            iframe.style.height = "100vh";
+        }else{
+            iframe.style.width = "500px";
+            img.style.bottom = "50px";
+            img.style.right = "50px";
+            iframe.style.height = "100vh";
+        }
 
         iframe.style.zIndex = 99999;
         iframe.style.border = "none";
         iframe.style.backgroundColor = "transparent";
         iframe.style.borderRadius = "5px";
 
-        iframe.onload = function () {
-
+        iframe.onload = function(){
+            
             iframe.contentWindow.startchat();
         }
 
@@ -43,4 +49,4 @@
     document.body.appendChild(img);
 
 
-})();
+ })();
