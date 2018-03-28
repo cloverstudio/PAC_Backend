b742f64d8c920e6731ac29c129334c46af49ee77
Jurica Blazevic <jurica.blazevic@clover-studio.com>
2018-03-28 08:54:45 +0200

    commit

--- src/server/WebAPI/Backend/Controllers/File/FileDownloadController.js
+++ src/server/WebAPI/Backend/Controllers/File/FileDownloadController.js
@@ -11,34 +11,34 @@ var path = require('path');
 
 var pathTop = "../../../../";
 
-var Const = require( pathTop + "lib/consts");
-var Config = require( pathTop + "lib/init");
-var DatabaseManager = require( pathTop + 'lib/DatabaseManager');
-var Utils = require( pathTop + 'lib/utils');
-var UserModel = require( pathTop + 'Models/User');
-var GroupModel = require( pathTop + 'Models/Group');
-var FileModel = require( pathTop + 'Models/File');
-var OrganizationModel = require( pathTop + 'Models/Organization');
+var Const = require(pathTop + "lib/consts");
+var Config = require(pathTop + "lib/init");
+var DatabaseManager = require(pathTop + 'lib/DatabaseManager');
+var Utils = require(pathTop + 'lib/utils');
+var UserModel = require(pathTop + 'Models/User');
+var GroupModel = require(pathTop + 'Models/Group');
+var FileModel = require(pathTop + 'Models/File');
+var OrganizationModel = require(pathTop + 'Models/Organization');
 
 var BackendBase = require('../BackendBase');
 
-var FileDownloadController = function(){
+var FileDownloadController = function () {
 }
 
-_.extend(FileDownloadController.prototype,BackendBase.prototype);
+_.extend(FileDownloadController.prototype, BackendBase.prototype);
 
-FileDownloadController.prototype.init = function(app){
-        
+FileDownloadController.prototype.init = function (app) {
+
     var self = this;
 
-   /**
-     * @api {get} /api/v2/file/fileId download file
-     * @apiName File Download
-     * @apiGroup WebAPI
-     * @apiDescription Returns file binary
-     **/
+    /**
+      * @api {get} /api/v2/file/fileId download file
+      * @apiName File Download
+      * @apiGroup WebAPI
+      * @apiDescription Returns file binary
+      **/
 
-    router.get('/:fileId',function(request,response){
+    router.get('/:fileId', function (request, response) {
 
         var fileId = request.params.fileId;
         var filePath = Config.uploadPath + "/" + fileId;
@@ -50,50 +50,48 @@ FileDownloadController.prototype.init = function(app){
             (done) => {
 
                 fileModel.findOne({
-                    _id:fileId
-                },function(err,findResult){
+                    _id: fileId
+                }, function (err, findResult) {
 
-                    done(err,{
-                        fileModel:findResult
+                    done(err, {
+                        fileModel: findResult
                     })
                 });
 
             },
-            (result,done) => {
+            (result, done) => {
 
                 fs.exists(filePath, function (exists) {
-                    
-                    if(!exists){
-                        
-                        done("file not found",result);
 
-                        
-                        
+                    if (!exists) {
+
+                        done("file not found", result);
+
+
+
                     } else {
-                        
+
                         result.filePath = filePath;
-                        done(null,result);
-                        
-                        
+                        done(null, result);
+
+
                     }
-                    
+
                 });
 
             },
 
-        ],(err, result) => {
+        ], (err, result) => {
 
-            if(err){
-                response.send('File Not Found', 404);
-                return;
-            }
+            if (err)
+                return response.status(404).send('File Not Found');
 
-            response.download(filePath,result.fileModel.name);
+            response.download(filePath, result.fileModel.name);
 
         });
 
     });
-    
+
     return router;
 
 }
